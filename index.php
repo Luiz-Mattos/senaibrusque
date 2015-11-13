76a4f4bc605e6a3f8e53b48ff48b88f8ff1dce8a
luiz-mattos <luiz.mattos1@hotmail.com>
2015-11-13 20:40:58 -0200

    Closes #2

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lan="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Site Institucional do SENAI - Brusque</title>
+    </head>
+    <body>
+        <?php
+        
+        ?>
+    </body>
+</html>
