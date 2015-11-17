4f5e6a2d27963f79d0b8baaaec04018421d9ed43
Luiz <luiz-mattos1@hotmail.com>
2015-11-17 00:24:43 -0200

    Menu, galera de imagens, seção dos cursos - Aula 17/11/2015

--- index.php
+++ index.php
@@ -22,19 +22,128 @@
                     <form role="form">
                         <div class="form-inline">
                             <div class="input-group input-group-sm">
-                                
-                            <span class="input-group-addon">
-                                <span class="glyphicon glyphicon-envelope"></span>
-                            </span>
-                            <input type="email" class="form-control" id="email" placeholder="Digite seu e-mail" >
-                            </div>
-                        
-                        <button type="submit" class="btn btn-default">Cadastrar</button>
-                        </div>
-                    </form>
-                </div>
 
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-envelope"></span>
+                                </span>
+                                <input type="email" class="form-control" id="email" placeholder="Digite seu e-mail" >
+                            </div>
+
+                            <button type="submit" class="btn btn-default">Cadastrar</button>
+                        </div>
+
+                </div>
+                </form>
             </header>
         </div>
+        <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="250">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                        
+                    </button>
+                    <a class="navbar-brand" href="#">SENAI</a>
+                </div>
+                <div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav">
+                            <li><a href="#section1">Início</a></li>
+                            <li><a href="#section2">Cursos</a></li>
+                            <li><a href="#section3">Notícias</a></li>
+                            <li><a href="#section4">Fotos</a></li>
+                            <li><a href="#section5">Vídeos</a></li>
+                            <li><a href="#section6">Contato</a></li>
+                            <li><a href="#section7">Localização</a></li>
+
+                            <!-- Menu drop-down
+                            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Section 4 <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#section41">Section 4-1</a></li>
+                                    <li><a href="#section42">Section 4-2</a></li>
+                                </ul>
+                            </li>
+                            -->
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>    
+
+        <div id="section1" class="container-fluid">
+            <h1>Início</h1>
+            <!-- Carrossel de imagens -->
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="http://static.sites.yp.com/gfx/freeTextImage/placeholder.png" alt="Chania">
+                    </div>
+
+                    <div class="item">
+                        <img src="http://www.jvs.org/wordpress/wp-content/uploads/2013/09/placeholder.png" alt="Chania">
+                    </div>
+
+                    <div class="item">
+                        <img src="http://vignette1.wikia.nocookie.net/wikiaglobal/images/1/12/Placeholder_location.png/revision/latest?cb=20100112182400" alt="Flower">
+                    </div>
+
+                    <div class="item">
+                        <img src="http://streetmyanmar.com/gallery_photo/php/files/thumbnail/default.png" alt="Flower">
+                    </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+            <!-- fim do carrossel de imagens -->
+        </div>
+        <div id="section2" class="container-fluid">
+            <h1>Cursos</h1>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        </div>
+        <div id="section3" class="container-fluid">
+            <h1>Notícias</h1>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        </div>
+        <div id="section4" class="container-fluid">
+            <h1>Fotos</h1>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        </div>
+        <div id="section5" class="container-fluid">
+            <h1>Vídeos</h1>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        </div>
+        <div id="section6" class="container-fluid">
+            <h1>Contato</h1>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        </div>
+        <div id="section7" class="container-fluid">
+            <h1>Localização</h1>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        </div>
+        <script src="js/meuscript.js"></script>
     </body>
 </html>
