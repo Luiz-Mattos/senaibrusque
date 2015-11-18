d834bbeaea778549e0bbb2ad3658b90a524ed9df
luiz <luiz.mattos1@hotmail.com>
2015-11-18 21:48:19 -0200

    Cursos e fotos

--- index.php
+++ index.php
@@ -17,25 +17,22 @@
             <header class="jumbotron text-center" id="topo">
                 <h1>SENAI/SC Brusque</h1>
                 <p>Alo mundo do bootstrap</p>
-                <div class="container">
-
-                    <form role="form">
-                        <div class="form-inline">
-                            <div class="input-group input-group-sm">
-
-                                <span class="input-group-addon">
-                                    <span class="glyphicon glyphicon-envelope"></span>
-                                </span>
-                                <input type="email" class="form-control" id="email" placeholder="Digite seu e-mail" >
-                            </div>
-
-                            <button type="submit" class="btn btn-default">Cadastrar</button>
-                        </div>
-
+                
+            <form role="form" class="form-inline">
+                <!-- Gerador de Bootstrap Form 
+                     http://embed.plnkr.co/ZrnnkFR3Yv9HGQHrRMiW/preview -->
+                <!-- Em caso de sucesso na validação: <div class="form-group has-feedback has-success"> -->
+                <!-- Em caso de falha na validação: <div class="form-group has-feedback has-error"> -->
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control" placeholder="seunome@provedor.com.br" size="40px"/>
+                    <i class="form-control-feedback glyphicon glyphicon-envelope"></i>
                 </div>
-                </form>
-            </header>
-        </div>
+                <button type="submit" class="btn btn-success">Cadastrar</button>
+            </form>
+        </header>
+
+        <!-- Barra de Navegação -->
+
         <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="250">
             <div class="container-fluid">
                 <div class="navbar-header">
@@ -49,101 +46,172 @@
                 <div>
                     <div class="collapse navbar-collapse" id="myNavbar">
                         <ul class="nav navbar-nav">
-                            <li><a href="#section1">Início</a></li>
-                            <li><a href="#section2">Cursos</a></li>
-                            <li><a href="#section3">Notícias</a></li>
-                            <li><a href="#section4">Fotos</a></li>
-                            <li><a href="#section5">Vídeos</a></li>
-                            <li><a href="#section6">Contato</a></li>
-                            <li><a href="#section7">Localização</a></li>
+                            <li><a href="#inicio">Início</a></li>
+                            <li><a href="#cursos">Cursos</a></li>
+                            <li><a href="#noticias">Notícias</a></li>
+                            <li><a href="#fotos">Fotos</a></li>
+                            <li><a href="#videos">Vídeos</a></li>
+                            <li><a href="#contato">Contato</a></li>
+                            <li><a href="#local">Localização</a></li>
 
-                            <!-- Menu drop-down
-                            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Section 4 <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#section41">Section 4-1</a></li>
-                                    <li><a href="#section42">Section 4-2</a></li>
-                                </ul>
-                            </li>
-                            -->
                         </ul>
                     </div>
                 </div>
             </div>
         </nav>    
+        <!-- Fim da Barra de Navegação -->
 
-        <div id="section1" class="container-fluid">
-            <h1>Início</h1>
+        <section id="inicio" class="container-fluid">
+
             <!-- Carrossel de imagens -->
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#myCarousel" data-slide-to="1"></li>
-                    <li data-target="#myCarousel" data-slide-to="2"></li>
-                    <li data-target="#myCarousel" data-slide-to="3"></li>
-                </ol>
+            <div clas="row">                
+                <!--
+                Uma linha dividida em 12 colunas, sendo uma coluna central
+                tamanho 8 e um distanciamento de 2
+                
+                Sistema de Grid no Bootstrap
+                http://www.w3schools.com/bootstrap/bootstrap_grid_examples.asp
+                -->
+                <div class="col-lg-8 col-lg-offset-2">
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel" data-pause="hover" data-interval="2000">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                            <li data-target="#myCarousel" data-slide-to="2"></li>
+                            <li data-target="#myCarousel" data-slide-to="3"></li>
+                        </ol>
 
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner" role="listbox">
-                    <div class="item active">
-                        <img src="http://static.sites.yp.com/gfx/freeTextImage/placeholder.png" alt="Chania">
-                    </div>
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <img src="img/saiba_mais_sobre_o_senai.png" alt="Chania">
+                            </div>
 
-                    <div class="item">
-                        <img src="http://www.jvs.org/wordpress/wp-content/uploads/2013/09/placeholder.png" alt="Chania">
-                    </div>
+                            <div class="item">
+                                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=460%C3%97365&w=460&h=365" alt="Chania">
+                            </div>
 
-                    <div class="item">
-                        <img src="http://vignette1.wikia.nocookie.net/wikiaglobal/images/1/12/Placeholder_location.png/revision/latest?cb=20100112182400" alt="Flower">
-                    </div>
+                            <div class="item">
+                                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=460%C3%97365&w=460&h=365" alt="Flower">
+                            </div>
 
-                    <div class="item">
-                        <img src="http://streetmyanmar.com/gallery_photo/php/files/thumbnail/default.png" alt="Flower">
+                            <div class="item">
+                                <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=460%C3%97365&w=460&h=365" alt="Flower">
+                            </div>
+                        </div>
+
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
                     </div>
                 </div>
-
-                <!-- Left and right controls -->
-                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
+            </div> <!-- row -->
             <!-- fim do carrossel de imagens -->
-        </div>
-        <div id="section2" class="container-fluid">
+        </section>
+        <section id="cursos" class="container-fluid">
             <h1>Cursos</h1>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-        <div id="section3" class="container-fluid">
+
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p>Curso 1</p>
+                        <button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal">Mais informações</button>
+                    </div>
+                </div>
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Curso 1</h4>
+      </div>
+      <div class="modal-body">
+        <p>Curso 1.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+            </div>
+            
+            
+            
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p>Curso 2</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p>Curso 3</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p>Curso 4</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </section>
+        <section id="noticias" class="container-fluid">
             <h1>Notícias</h1>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-        <div id="section4" class="container-fluid">
+        </section>
+        <section id="fotos" class="container-fluid">
             <h1>Fotos</h1>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-            <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-        <div id="section5" class="container-fluid">
+<div class="row">
+  <div class="col-xs-6 col-md-3">
+    <a href="http://ichef-1.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/04/12/150412094505_chipmunk_624x351_bbc_nocredit.jpg" class="thumbnail">
+      <img img src="http://ichef-1.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/04/12/150412094505_chipmunk_624x351_bbc_nocredit.jpg" alt="Chania">
+    </a>
+  </div>
+<div class="row">
+  <div class="col-xs-6 col-md-3">
+    <a href="http://www.emitir.com.br/wp-content/uploads/2013/09/lindas-imagens-Papagaio.jpg" class="thumbnail">
+      <img src="http://www.emitir.com.br/wp-content/uploads/2013/09/lindas-imagens-Papagaio.jpg" alt="img2">
+    </a>
+  </div>
+  ...
+</div>
+</div>
+            
+        </section>
+        <section id="videos" class="container-fluid">
             <h1>Vídeos</h1>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-        <div id="section6" class="container-fluid">
+        </section>
+        <section id="contato" class="container-fluid">
             <h1>Contato</h1>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
-        <div id="section7" class="container-fluid">
+        </section>
+        <section id="local" class="container-fluid">
             <h1>Localização</h1>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
             <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        </div>
+        </section>
         <script src="js/meuscript.js"></script>
     </body>
 </html>
