41cdb74527adb4a912eeb84a01f45d407daf065d
Aluno <Aluno@10.1.20.64>
2015-11-27 22:46:49 -0200

    newsletter

---
+++ newsletter.php
@@ -0,0 +1,16 @@
+<section id="newsletter" >
+
+        </div>
+
+                <form action="cadastro.php" method="post" role="form" class="form-inline" >
+                    <div class="form-group has-feedback">
+                        <input name="email" type="text" class="form-control" placeholder="seunome@provedor.com.br" size="40px"/>
+                        <i class="form-control-feedback glyphicon glyphicon-envelope"></i>
+                    </div>
+                    <button id="news" name="btn" type="submit" class="btn btn-success">Cadastrar</button>
+                </form>
+
+    </div>
+
+
+</section>
